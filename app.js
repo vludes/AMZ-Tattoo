88627744b6a33fd10b90c6c27fbcd90f9db3b6e4
Vladyslav Lototskyy <lototskyi.vladislav@gmail.com>
2022-01-14 22:13:37 +0200

    sticky

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ menuBtn.addEventListener('click', function () {
     menuBtn.classList.toggle('active');
     menu.classList.toggle('active');
 })
+
+
+window.onscroll = function () {
+    var top = window.scrollY;
+    console.log('Top: ' + top);
+    var header = document.getElementsByTagName('header');
+    var offset = header.innerHeight;
+    if (top > offset) {
+        header[0].classList.remove('top');
+        header[0].classList.add('scrolled');
+    } else {
+        header[0].classList.remove('scrolled');
+        header[0].classList.add('top');
+    }
+};
